bfcb1c7065b35182a1f28e7fd981cb9f2d419e46
Gustavo <gustavo.pnhro@gmail.com>
2024-03-24 21:38:49 -0300

    add: Adiciona mais comandos ao user_data.sh
    Adiciona os comandos de instalação do efs, ativação do docker, montagem do efs dentro do diretório /mnt/efs, instalação do docker-compose e execução do container existente dentro do efs

--- user_data.sh
+++ user_data.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 sudo yum update -y
-sudo yum docker install -y
+sudo yum install docker -y && sudo yum install amazon-efs-utils -y
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker $(whoami)
-sudo reboot
+sudo mkdir /mnt/efs
+sudo su
+echo "fs-0d4ccf2167f49f408.efs.us-east-1.amazonaws.com:/    /mnt/efs    nfs4    defaults,_netdev,rw    0   0" >  /etc/fstab 
+mount -a
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo mv /usr/local/bin/docker-compose /bin/docker-compose
+sudo docker-compose -f /mnt/efs/docker-compose.yml up -d
