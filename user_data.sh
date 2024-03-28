a4116e813dde3c662f9a5ee8543cbd2684657c03
Gustavo <gustavo.pnhro@gmail.com>
2024-03-28 10:16:24 -0300

    docs: Atualiza o user_data.sh para variáveis genéricas
    Atualiza o user_data.sh para usar variáveis genéricas para fins de segurança

--- user_data.sh
+++ user_data.sh
@@ -7,7 +7,7 @@ sudo systemctl start docker
 sudo systemctl enable docker
 sudo mkdir /mnt/efs
 sudo su
-sudo echo "fs-0d4ccf2167f49f408.efs.us-east-1.amazonaws.com:/    /mnt/efs    nfs4    defaults,_netdev,rw    0   0" >  /etc/fstab 
+sudo echo "<efs_dns>:/    /mnt/efs    nfs4    defaults,_netdev,rw    0   0" >  /etc/fstab 
 mount -a
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /bin/docker-compose
 sudo chmod +x /bin/docker-compose
@@ -21,10 +21,10 @@ services:
     ports:
       - 80:80
     environment:
-      WORDPRESS_DB_HOST: database-0.c5oe8kc447xs.us-east-1.rds.amazonaws.com
-      WORDPRESS_DB_USER: admin
-      WORDPRESS_DB_PASSWORD: wordpassword
-      WORDPRESS_DB_NAME: wordpress
+      WORDPRESS_DB_HOST: <RDS endpoint>
+      WORDPRESS_DB_USER: <master user>
+      WORDPRESS_DB_PASSWORD: <master password>
+      WORDPRESS_DB_NAME: <initial database>
       WORDPRESS_TABLE_CONFIG: wp_
 EOL
 sudo docker-compose -f /home/ec2-user/docker-compose.yml up -d
